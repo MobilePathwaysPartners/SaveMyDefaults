f903e27aba7f8790d8be5504bfd1bc04d019d4ba
William Skrzypczak <64371704+MobilePathwaysPartners@users.noreply.github.com>
2020-08-09 13:34:20 -0500

    First Commit

---
+++ BillsSaveMyDefaults/BillsSaveMyDefaults/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  BillsSaveMyDefaults
+//
+//  Created by Bill Skrzypczak on 2/2/20.
+//  Copyright © 2020 Bill Skrzypczak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func mySaveButton(_ sender: Any) {
+        
+    UserDefaults.standard.set(mySerialNumber.text, forKey: "Text")
+    UserDefaults.standard.set(mySwitch.isOn, forKey: "Switch")
+    UserDefaults.standard.set(mySlider.value, forKey: "Slider")
+    }
+    
+    @IBAction func myLoadButton(_ sender: Any) {
+        mySwitch.isOn = UserDefaults.standard.bool(forKey: "Switch")
+        mySlider.value = UserDefaults.standard.float(forKey: "Slider")
+        mySerialNumber.text = UserDefaults.standard.string(forKey: "Text")
+       
+    }
+    
+    @IBAction func myDeleteButton(_ sender: Any) {
+        mySwitch.isOn = true
+        mySlider.value = 0.5
+        mySerialNumber.text = ""
+        
+    }
+    
+
+    @IBOutlet var mySerialNumber: UITextField!
+    
+    
+    @IBOutlet var mySlider: UISlider!
+    
+    
+    
+    @IBOutlet var mySwitch: UISwitch!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
